c54ced684f5636f6fe6c3b5e89189e94fbf6611b
lapnour <60771751+lapnour@users.noreply.github.com>
2022-01-04 16:31:59 +0800

    Update voc_faster_rcnn_r101.py

--- configs/faster_rcnn/voc_faster_rcnn_r101.py
+++ configs/faster_rcnn/voc_faster_rcnn_r101.py
@@ -1,6 +1,6 @@
 model = dict(
     type='FasterRCNN',
-    pretrained='/tmp/ImageNet-pretrained/resnet101-5d3b4d8f.pth',#缺少，需要修改
+    pretrained='/tmp/ImageNet-pretrained/resnet101-5d3b4d8f.pth',#缺少，需要修改（mm下没有）
     backbone=dict(
         type='ResNet',
         depth=101,
@@ -115,7 +115,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),#COCO img_scale=(1333, 800)
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -144,12 +144,14 @@ data = dict(
         type='RepeatDataset',
         times=3,
         dataset=dict(
-            type=dataset_type,
+            type=dataset_type,#type='CocoDataset'
             ann_file=[
                 data_root + 'VOC2007/ImageSets/Main/trainval.txt',
                 data_root + 'VOC2012/ImageSets/Main/trainval.txt'
             ],
             img_prefix=[data_root + 'VOC2007/', data_root + 'VOC2012/'],
+            #ann_file='data/VOC_COCO/annotations/instances_train2017.json',
+            #img_prefix='data/VOC_COCO/images/',
             pipeline=train_pipeline)),
     val=dict(
         type=dataset_type,
@@ -162,6 +164,7 @@ data = dict(
         img_prefix=data_root + 'VOC2007/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='mAP')
+#evaluation = dict(interval=1, metric='bbox')
 # optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
