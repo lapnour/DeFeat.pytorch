3f61a753aea6d55d5034b48a6f55fdade1aca4fc
lapnour <60771751+lapnour@users.noreply.github.com>
2022-01-05 17:30:22 +0800

    Update builder.py

--- mmdet/models/builder.py
+++ mmdet/models/builder.py
@@ -1,3 +1,8 @@
+#build_detector 函数将配置文件中的：model、train_cfg 和 test_cfg 传入参数。
+#运行时会将上面的三个值作为参数传入 build_detector 函数，
+    #build_detector 函数会调用 build 函数
+    #build 函数调用 build_from_cfg 函数构建检测器对象
+    #其中 train_cfg 和 test_cfg 作为默认参数用于构建 detector 对象。
 from mmcv.utils import Registry, build_from_cfg
 from torch import nn
 
@@ -17,6 +22,7 @@ def build(cfg, registry, default_args=None):
         ]
         return nn.Sequential(*modules)
     else:
+        # 调用 build_from_cfg 用来根据 config 字典构建 registry 里面的对象
         return build_from_cfg(cfg, registry, default_args)
 
 
@@ -45,4 +51,6 @@ def build_loss(cfg):
 
 
 def build_detector(cfg, train_cfg=None, test_cfg=None):
+    # 调用 build 函数，传入 cfg, registry 对象，
+    # 把 train_cfg 和 test_cfg 作为默认字典传入
     return build(cfg, DETECTORS, dict(train_cfg=train_cfg, test_cfg=test_cfg))
